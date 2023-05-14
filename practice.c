476a2b6d39d721359227fac8af71cf91ee00a504
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-05-14 20:53:46 +0530

    edit

--- practice.c
+++ practice.c
@@ -170,44 +170,295 @@ isosceles, equilateral, scalene or right angled triangle.*/
 
 // Prime numbers
 
-#include <stdio.h>
+// #include <stdio.h>
 
-void check_prime(int num)
-{
+// void check_prime(int num)
+// {
 
-    int prime = 1;
-    for (int i = 2; i <= num / 2; i++)
-    {
-        if (num % i == 0)
-        {
-            printf("The number is not prime.\n");
-            prime = 0;
-            break;
-        }
-    }
-    if (prime)
-    {
-        printf("The number is prime.\n");
-    }
-}
+//     int prime = 1;
+//     for (int i = 2; i <= num / 2; i++)
+//     {
+//         if (num % i == 0)
+//         {
+//             printf("The number is not prime.\n");
+//             prime = 0;
+//             break;
+//         }
+//     }
+//     if (prime)
+//     {
+//         printf("The number is prime.\n");
+//     }
+// }
 
-int main()
-{
-    int num;
-    printf("Enter a number to check for prime.\n");
-    scanf("%d", &num);
-    if (num == 1)
-    {
-        printf("1 is not a prime or composite number.\n");
-        printf("Enter a number to check for prime.\n");
-        scanf("%d", &num);
-    }
+// int main()
+// {
+//     int num;
+//     printf("Enter a number to check for prime.\n");
+//     scanf("%d", &num);
+//     if (num == 1)
+//     {
+//         printf("1 is not a prime or composite number.\n");
+//         printf("Enter a number to check for prime.\n");
+//         scanf("%d", &num);
+//     }
 
-    check_prime(num);
+//     check_prime(num);
 
-    return 0;
-}
+//     return 0;
+// }
 
-/*Write a program to print all the ASCII values and their
-equivalent characters using a while loop. The ASCII values
-vary from 0 to 255.*/
+/*(a) Ramesh’s basic salary is input through the keyboard. His
+dearness allowance is 40% of basic salary, and house rent
+allowance is 20% of basic salary. Write a program to calculate
+his gross salary.*/
+
+// #include <stdio.h>
+
+// void gross_salary(int salary)
+// {
+//     int gross = salary + (0.4 * salary) + (0.2 * salary);
+//     printf("Your gross salary is Rs.%d\n", gross);
+// }
+// int main()
+// {
+//     int salary;
+//     printf("Enter your basic salary.\n");
+//     scanf("%d", &salary);
+//     gross_salary(salary);
+
+//     return 0;
+// }
+
+/*(b) The distance between two cities (in km.) is input through the
+keyboard. Write a program to convert and print this distance
+in meters, feet, inches and centimeters.*/
+// #include <stdio.h>
+
+// int main()
+// {
+//     float distance;
+//     printf("Enter the distance in kilometers.\n");
+//     scanf("%f", &distance);
+//     printf("The distance in meters is : %.1lf meter. \n", distance * 1000);
+//     printf("The distance in feet is : %.1lf feet. \n", distance * 3280.84);
+//     printf("The distance in inches is : %.1lf inch. \n", distance * 39370.1);
+//     printf("The distance in cm is : %.1lf cm. \n", distance * 1000000);
+//     return 0;
+// }
+
+/*If the marks obtained by a student in five different subjects
+are input through the keyboard, find out the aggregate marks
+and percentage marks obtained by the student. Assume that
+the maximum marks that can be obtained by a student in each
+subject is 100.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     float sub1, sub2, sub3, sub4, sub5;
+//     printf("Enter your marks.\n");
+//     scanf("%f%f%f%f%f", &sub1, &sub2, &sub3, &sub4, &sub5);
+//     float aggregate = sub1 + sub2 + sub3 + sub4 + sub5;
+//     float percentage = (aggregate / 5);
+//     printf("The aggregate is %.2f and the percentage is %.2f\%%\n", aggregate, percentage);
+
+//     return 0;
+// }
+
+/*(d) Temperature of a city in Fahrenheit degrees is input through
+the keyboard. Write a program to convert this temperature
+into Centigrade degrees.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     float temp, temp_c;
+//     printf("Please enter the temperature in fahrenheit.\n");
+//     scanf("%f", &temp);
+//     temp_c = (temp - 32) * (5.0 / 9.0);
+//     printf("The temperature in centigrade is %.2f C.\n", temp_c);
+
+//     return 0;
+// }
+
+/*(e) The length & breadth of a rectangle and radius of a circle are
+input through the keyboard. Write a program to calculate the
+area & perimeter of the rectangle, and the area &
+circumference of the circle.*/
+
+// #include <stdio.h>
+
+// int area(float length, float breadth)
+// {
+//     return length * breadth;
+// }
+// int perimeter(float length, float breadth)
+// {
+//     return 2.0 * (length + breadth);
+// }
+
+// int circumference(float radius)
+// {
+//     return 2.0 * 3.14 * radius;
+// }
+
+// int main()
+// {
+
+//     float length, breadth, radius;
+//     printf("Please enter the length, breadth and radius.\n");
+//     scanf("%f%f%f", &length, &breadth, &radius);
+//     printf("The area is: %.2f\n", area(length, breadth));
+//     printf("The perimeter is: %.2f\n", perimeter(length, breadth));
+//     printf("The circumference is: %.2f\n", circumference(radius));
+
+//     return 0;
+// }
+
+/*(f) Two numbers are input through the keyboard into two
+locations C and D. Write a program to interchange the
+contents of C and D.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int C, D;
+//     printf("Enter the values.\n");
+//     scanf("%d%d", &C, &D);
+//     int temp = C;
+//     C = D;
+//     D = temp;
+//     printf("C=%d  D=%d\n", C, D);
+
+//     return 0;
+// }
+
+/*(g) If a five-digit number is input through the keyboard, write a
+program to calculate the sum of its digits.*/
+
+// #include <stdio.h>
+// int main()
+// {
+//     int number, sum = 0;
+//     printf("Enter the number.\n");
+//     scanf("%d", &number);
+//     for (int i = 0; i < 5; i++)
+//     {
+//         sum += number % 10;
+//         number = number / 10;
+//     }
+//     printf("The sum of the digist of the number is: %d\n", sum);
+
+//     return 0;
+// }
+
+/*(h) If a five-digit number is input through the keyboard, write a
+program to reverse the number.*/
+
+// #include <stdio.h>
+
+// void reverse(int number)
+// {
+//     int reversed_num = 0;
+//     while (number)
+//     {
+//         reversed_num = reversed_num * 10 + number % 10;
+//         number = number / 10;
+//     }
+//     printf("The reversed number is: %d\n", reversed_num);
+// }
+// int main()
+// {
+//     int number;
+//     printf("Enter a number.\n");
+//     scanf("%d", &number);
+//     reverse(number);
+
+//     return 0;
+// }
+
+/*(i) If a four-digit number is input through the keyboard, write a
+program to obtain the sum of the first and last digit of this
+number.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num;
+//     printf("Enter the number.\n");
+//     scanf("%d", &num);
+//     int num1 = num / 1000, num4 = num % 10;
+//     printf("The sum of %d and %d is : %d\n", num1, num4, num1 + num4);
+
+//     return 0;
+// }
+
+/*(k) A cashier has currency notes of denominations 10, 50 and
+100. If the amount to be withdrawn is input through the
+keyboard in hundreds, find the total number of currency notes
+of each denomination the cashier will have to give to the
+withdrawer.*/
+
+// #include <stdio.h>
+// int main()
+// {
+//     int amt;
+//     printf("Please enter the amount.\n");
+//     scanf("%d", &amt);
+//     printf("In 10's: %d\n", amt / 10);
+//     printf("In 50's: %d\n", amt / 50);
+//     printf("In 100's: %d\n", amt / 100);
+
+//     return 0;
+// }
+
+/*While purchasing certain items, a discount of 10%
+is offered if the quantity purchased is more than 1000. If quantity
+and price per item are input through the keyboard, write a program
+to calculate the total expenses.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int dis = 0, qty, rate, total;
+//     printf("Enter the quantity and the rate.\n");
+//     scanf("%d%d", &qty, &rate); // Corrected scanf statement
+//     if (qty > 1000)
+//     {
+//         dis = 10;
+//     }
+//     total = qty * rate - qty * rate * (dis / 100);
+//     printf("The total is: %d\n", total);
+
+//     return 0;
+// }
+
+/*The current year and the year in which the
+employee joined the organization are entered through the
+keyboard. If the number of years for which the employee has
+served the organization is greater than 3 then a bonus of Rs. 2500/-
+is given to the employee. If the years of service are not greater
+than 3, then the program should do nothing.*/
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int current_year, year_joined, service, bonus;
+//     printf("Enter the values.\n");
+//     scanf("%d%d", &current_year, &year_joined);
+//     service = current_year - year_joined;
+//     if (service > 3)
+//     {
+//         bonus = 2500;
+//         printf("Bonus = %d\n", bonus);
+//     }
+
+//     return 0;
+// }
