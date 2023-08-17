fea6a4fd0d3b9cfdf9de768a16ac1476eaeec0fd
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-08-17 07:32:42 +0530

    recursive function

--- questions/taylor_series_horners_rule.c
+++ questions/taylor_series_horners_rule.c
@@ -2,13 +2,25 @@
 
 
 //using for-loop
-double e(double x,double n){
-    double s=1;
+// double e(double x,double n){
+//     double s=1;
     
-    for(;n>0;n--){
-        s = 1+x/n*s;
-    }
-    return s; 
+//     for(;n>0;n--){
+//         s = 1+x/n*s;
+//     }
+//     return s; 
+// }
+
+//using recursion
+double e(double x,double n){
+    static double s=1;
+    
+    if(n==0)
+        return s;
+    
+    s = 1+x/n*s;
+    
+    return e(x, n-1); 
 }
 
 int main(){
