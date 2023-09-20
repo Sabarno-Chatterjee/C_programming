ec584e103565c9555f35f161a1d03c7070f7ef96
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-20 20:54:09 +0530

    edit

--- practice.c
+++ practice.c
@@ -197,12 +197,7 @@
 #include<stdio.h>
 
 int fib(int n){
-    if(n==0)
-        return 0;
-    else if (n==1)
-        return 1;
-    else    
-        return fib(n-1)+ fib(n-2);      
+    return (n==0 || n==1) ? n : fib(n-1)+ fib(n-2);          
 }
 
 int main(){
