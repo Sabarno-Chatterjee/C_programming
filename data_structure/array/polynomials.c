81b3c174c733c2642b28c0cd0c9a101691e1eec8
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-01-08 19:05:26 +0530

    edit

---
+++ data_structure/array/polynomials.c
@@ -0,0 +1,28 @@
+//addition of polynomials
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct term{
+    int coefficient ;
+    int exponent;
+};
+
+struct build_polynomial{
+    int num_of_terms;
+    struct term *terms;
+};
+
+void create_polynomial(struct build_polynomial *P){
+    printf("Enter the number of terms\n");
+    scanf("%d",&P->num_of_terms);
+    P->terms=(struct term*)malloc(P->num_of_terms*sizeof(struct term));
+    printf("Enter the coefficent and exponent");
+    for(int i=0;i<P->num_of_terms;i++)
+        scanf("%d%d",&P->terms[i].coefficient,&P->terms[i].exponent);
+}
+
+int main(){
+    
+    return 0;
+}
