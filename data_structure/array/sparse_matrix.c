74961e8208f3a669eed4b3fb763c405ef8818c39
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-01-08 15:02:31 +0530

    edit

--- data_structure/array/sparse_matrix.c
+++ data_structure/array/sparse_matrix.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct elements{
     int i;      //row
@@ -7,14 +8,22 @@ struct elements{
 };
 
 struct sparse_matrix{
-    int m;      //row
-    int n;      //column
-    int data;   //non zero element
+    int row;      //row
+    int column;      //column
+    int num_non_zero;   //non zero element
     struct elements *element;       //self referential data structure to store array of non zero elements dynamically
 };
 
-void create_sparse(struct sparse_matrix *S, int size){
+void create_sparse(struct sparse_matrix *S){
+    printf("Enter the dimensions of the matrix\n");
+    scanf("%d%d",&S->row,&S->column);
+    printf("Enter the number of non zero elements present\n");
+    scanf("%d",&S->num_non_zero);
     
+    S->element=(struct elements*)malloc(S->num_non_zero*(sizeof(struct elements)));
+    printf("Enter the data\n");
+    for(int i=0;i<S->num_non_zero;i++)
+        scanf("%d%d%d",&S->element[i].i,&S->element[i].j,&S->element[i].x);
 }
 
 int main(){
