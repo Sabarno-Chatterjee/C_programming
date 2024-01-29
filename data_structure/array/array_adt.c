23b196dfaac23efcb322a0df88276386f28aece8
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-01-29 19:28:22 +0530

    edit

--- data_structure/array/array_adt.c
+++ data_structure/array/array_adt.c
@@ -39,12 +39,71 @@ int Delete(struct Array *arr,int index){
     return 0;
 }
 
+void Swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int LinearSearch(struct Array *arr,int key){
+    for(int i=0;i<arr->length;i++){
+        if(arr->A[i]==key){
+            if(i-1>=0)
+                //improving linear search using transposition
+                // Swap(&arr->A[i],&arr->A[i-1]);
+                //improving linear search using move to head so that next search for the same element is performed faster
+                Swap(&arr->A[i],&arr->A[0]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+//iterative version
+int BinarySearch(struct Array arr,int key){
+    int low=0,high=arr.length-1,mid;
+    
+    while(low<=high){
+        mid=(low+high)/2;
+        if(key==arr.A[mid])
+            return mid;
+        else if(key<arr.A[mid])
+            high=mid-1;
+        else 
+            low=mid+1;
+    }
+    return -1;
+}
+
+
+//recursive version[although iterative is preffered as it is an eg of tail recursion]
+int RecursiveBinarySearch(int a[],int low,int high,int key){
+    int mid;
+    if(low<=high){
+        mid=(low+high)/2;
+        if(key==a[mid])
+            return mid;
+        else if(key<a[mid])
+            return RecursiveBinarySearch(a,low,mid-1,key);
+        else
+            return RecursiveBinarySearch(a,mid+1,high,key);
+    }
+    return -1;
+}
+
 int main(){
     int n;
     struct Array arr={{2,3,4,5,6},10,5};
-    Append(&arr,78);
-    Insert(&arr,4,9);
-    printf("\n%d\n",Delete(&arr,5));
+    // Append(&arr,78);
+    // Insert(&arr,4,9);
+    // printf("\n%d\n",Delete(&arr,5));
+    // printf("%d\n",LinearSearch(&arr,4));
+    // printf("%d\n",BinarySearch(arr,6));
+    printf("%d\n",RecursiveBinarySearch(arr.A,0,arr.length-1,2));
+    
+    
     Display(arr);
     
     
