35262ebf15602654edb2bf1759368aa1500f2487
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-02-01 15:30:23 +0530

    edit

--- data_structure/queue/queue_using_array.c
+++ data_structure/queue/queue_using_array.c
@@ -1,3 +1,5 @@
+//queue using array
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -8,25 +10,26 @@ struct Queue{
     int *Q;
 };
 
-void create(struct Queue *q, int size){
-    q->size=size;
-    q->front=q->rear=-1;
-    q->Q=(int *)malloc(q->size*sizeof(int));
+void create(struct Queue *q){
+    printf("Enter the size\n");
+    scanf("%d",&q->size);
+    q->Q=(int*)malloc(q->size*sizeof(int));
+    q->rear=q->front=-1;
 }
 
-void enqueue(struct Queue *q, int x){
+void enqueue(struct Queue *q,int info){
     if(q->rear==q->size-1)
-        printf("Queue is full.\n");
+        printf("Queue is full\n");
     else{
         q->rear++;
-        q->Q[q->rear]=x;
+        q->Q[q->rear]=info;
     }
 }
 
 int dequeue(struct Queue *q){
     int x=-1;
     if(q->front==q->rear)
-        printf("Queue is empty.\n");
+        printf("Queue is empty\n");
     else{
         q->front++;
         x=q->Q[q->front];
@@ -34,20 +37,20 @@ int dequeue(struct Queue *q){
     return x;
 }
 
-void display(struct Queue *q){
-    for(int i=q->front+1;i<=q->rear;i++){
-        printf("%d ",q->Q[i]);
-    }
+void display(struct Queue q){
+    for(int i=q.front+1;i<=q.rear;i++)
+        printf("%d ",q.Q[i]);
     printf("\n");
 }
 
 int main(){
     struct Queue q;
-    create(&q,5);
-    enqueue(&q,10);
-    enqueue(&q,12);
-    enqueue(&q,57);
-    display(&q);
-    printf("%d",dequeue(&q));
+    create(&q);
+    enqueue(&q,7);
+    enqueue(&q,8);
+    enqueue(&q,9);
+    dequeue(&q);
+    display(q);
+    
     return 0;
 }
