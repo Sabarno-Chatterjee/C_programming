65713caf38d6c580e63004469c06fa93c1b27fc5
Sabarno Chatterjee <sabarno.05@gmail.com>
2024-02-01 07:21:25 +0530

    edit

--- data_structure/linked_list/singly_linked_list.c
+++ data_structure/linked_list/singly_linked_list.c
@@ -4,57 +4,70 @@
 
 struct Node{
     int data;
-    struct Node *next;
-}*first=NULL,*last=NULL;
+    struct Node *next;              //self referential DS
+}*first=NULL;                       //global pointer to track the linked list
 
-int Count(struct Node *p){
+
+void create(int arr[],int n){
+    struct Node *temp,*last;
+    first=(struct Node*)malloc(sizeof(struct Node));
+    first->data=arr[0];
+    first->next=NULL;
+    last=first;
+    
+    for(int i=1;i<n;i++){
+        temp=(struct Node*)malloc(sizeof(struct Node));
+        temp->data=arr[i];
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+    }
+}
+
+int Length(struct Node *p){
     int c=0;
     while(p!=NULL){
         c++;
         p=p->next;
     }
-        
     return c;
 }
 
 void Insert(struct Node *p,int index,int info){
     struct Node *temp;
-    if(index<0 || index>Count(p))
-        return;
     
-    temp=(struct Node*)malloc(sizeof(struct Node*));
-    temp->data=info;
+    if(index<0 && index>Length(p))
+        return;
         
+    temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=info;
+    
     if(index==0){
         temp->next=p;
         first=temp;
     }
     else{
-        for(int i=0;i<index-1 && p;i++)
+        for(int i=0;i<index-1;i++)
             p=p->next;
         
         temp->next=p->next;
         p->next=temp;
     }
-    last=(struct Node*)malloc(sizeof(struct Node*));
-    last=p;
 }
 
-
 int Delete(struct Node *p,int index){
-    struct Node *q=NULL;
     int x=-1;
+    struct Node *q=NULL;
     
-    if(index<1 || index>Count(p))
-        return -1;
+    if(index<1 || index>Length(p))
+        return x;
+    
     if(index==1){
-        p=first;
         x=first->data;
         first=first->next;
         free(p);
     }
     else{
-        
         for(int i=0;i<index-1;i++){
             q=p;
             p=p->next;
@@ -70,24 +83,23 @@ int Delete(struct Node *p,int index){
 
 void display(struct Node *p){
     while(p!=NULL){
-        printf("%d ",p->data);
-        p=p->next;
+        printf("%d ",p->data);      //printing the data present in the current node
+        p=p->next;                  //traversing to new node
     }
+    printf("\n");                   //new line
 }
 
-int main(){ 
-    // int arr[]={1,2,3,4,5,6,7,8,9};
-    // int n=sizeof(arr)/sizeof(arr[0]);
-    // for(int i=0;i<n;i++)
-    //     Insert(first,i,arr[i]);
-    Insert(first,0,5);
-    Insert(first,1,9);
-    Insert(first,0,4);
-    Insert(first,3,45);
-    // printf("%d\n",Delete(first,1));
-    // printf("%d\n",Delete(first,1));
+int main(){
+    // int Arr[]={7,4,3,2,6,7,9};
+    // // int n=sizeof(Arr)/sizeof(Arr[0]);
+    // create(Arr,7);
+    //67,68,69,70
+    Insert(first,0,68);
+    Insert(first,1,69);
+    Insert(first,2,70);
+    Insert(first,0,67);
+    printf("%d\n",Delete(first,1));
     
-    printf("%d\n",Delete(first,2));
     display(first);
     
     return 0;
